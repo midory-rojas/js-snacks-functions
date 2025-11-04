78ddb944d9a907035bec676a1fb1ba889107478f
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-04 18:14:28 +0100

    feat:fatto esercizio 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,18 +5,30 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
-function vocali (stringa) {
-    let risultato ="";
+function contaVocali(stringa) {
+    let contatore = 0;
+    let vocaliTrovate = [];
 
-    for (let i=0; i < array.length; i++) {
-        const curElment = array[i];
-        console.log (curElment);
+    for (let i = 0; i < stringa.length; i++) {
+        let curLetter = stringa[i];
+
+        let aVocali = curLetter === "a";
+        let eVocali = curLetter === "e";
+        let iVocali = curLetter === "i";
+        let oVocali = curLetter === "o";
+        let uVocali = curLetter === "u";
+        if (aVocali || eVocali || iVocali || oVocali || uVocali)  { 
+            vocaliTrovate.push(curLetter);
+        }
+
     }
-        
+    return vocaliTrovate.length 
 }
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log(vocali(word));
+console.log(contaVocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
