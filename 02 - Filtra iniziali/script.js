bd61145554b267cd2d269991364f49a39928a215
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-04 16:36:26 +0100

    feat:esercizio numero 2 fatto

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,28 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che 
+iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
 
+function filtraLettera (array, lettera) {
+    /*console.log(array);*/
 
+    let nuovoArray = [];
+
+    for (let i=0; i < array.length; i++) {
+        const curName = array[i];
+        if (curName[0].toUpperCase() === lettera.toUpperCase()) {
+            nuovoArray.push(curName);
+        }
+    }
+    return nuovoArray;
+
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(filtraLettera(names, "A"));
+console.log(filtraLettera(names, "L"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,18 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vocali (stringa) {
+    let risultato ="";
+
+    for (let i=0; i < array.length; i++) {
+        const curElment = array[i];
+        console.log (curElment);
+    }
+        
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
